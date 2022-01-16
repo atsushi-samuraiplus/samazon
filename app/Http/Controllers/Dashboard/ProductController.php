a8bf6844002f3208a2710581aa392b56284e4c8b
atsushi-samuraiplus <kaisanof22@gmail.com>
2022-01-16 07:08:56 +0000

    14_2 recommended products display for top_page

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -84,6 +84,11 @@ class ProductController extends Controller
         $product->description = $request->input('description');
         $product->price = $request->input('price');
         $product->category_id = $request->input('category_id');
+        if ($request->input('recommend') == 'on') {
+            $product->recommend_flag = true;
+        } else {
+            $product->recommend_flag = false;
+        }
         $product->save();
 
         return redirect()->route('dashboard.products.index');
@@ -137,6 +142,11 @@ class ProductController extends Controller
         $product->description = $request->input('description');
         $product->price = $request->input('price');
         $product->category_id = $request->input('category_id');
+        if ($request->input('recommend') == 'on') {
+            $product->recommend_flag = true;
+        } else {
+            $product->recommend_flag = false;
+        }
         $product->update();
 
         return redirect()->route('dashboard.products.index');
