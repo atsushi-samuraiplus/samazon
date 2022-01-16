b156278d41c6f3dbb264fb033bd57d400b991170
atsushi-samuraiplus <kaisanof22@gmail.com>
2022-01-16 05:55:20 +0000

    13_2 new products display for top_page

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -68,6 +68,7 @@
             @endif
             
             <div class="modal fade" id="buy-confirm-modal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            
                <div class="modal-dialog">
                    <div class="modal-content">
                        <div class="modal-header">
