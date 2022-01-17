8298d5c7aadb843078ac003d36f89c65aed9e528
atsushi-samuraiplus <kaisanof22@gmail.com>
2022-01-17 05:48:44 +0000

    16 17_2 automatic shipping calculation function

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -89,6 +89,11 @@ class ProductController extends Controller
         } else {
             $product->recommend_flag = false;
         }
+        if ($request->input('carriage') == 'on') {
+            $product->carriage_flag = true;
+        } else {
+            $product->carriage_flag = false;
+        }
         $product->save();
 
         return redirect()->route('dashboard.products.index');
@@ -146,6 +151,11 @@ class ProductController extends Controller
             $product->recommend_flag = true;
         } else {
             $product->recommend_flag = false;
+        }
+        if ($request->input('carriage') == 'on') {
+            $product->carriage_flag = true;
+        } else {
+            $product->carriage_flag = false;
         }
         $product->update();
 
